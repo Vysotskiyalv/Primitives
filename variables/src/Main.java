5d47151992cbda677d380ddd1e38c0fb57e354e8
Alexey Vysotskiy <maracrus811@gmail.com>
2022-01-30 09:08:19 +0500

    initial commit

---
+++ variables/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int ticketprice = 200;
+    int rublesForBonusMile = 20;
+    int totalBonusMile = ticketprice/rublesForBonusMile;
+    System.out.println(totalBonusMile);
+  }
+}
+
